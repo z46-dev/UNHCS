72dcba5cd2d30995dd360af45bd09df276714652
KestutisBiskis <139379924+KestutisBiskis@users.noreply.github.com>
2024-11-18 18:24:35 +0000

    Create OddOrEven.js
    genius solution to solve odd or even

---
+++ OddOrEven.js
@@ -0,0 +1,16 @@
+// i havent done js in a while some syntax is probably wrong
+
+function isEven(n){
+  return !isOdd(n);
+}
+
+function isOdd(n){
+  return !isEven(n);
+}
+
+function main(){
+  println("Enter a number");
+  num = readLine();
+  print("number is even" + inEven(num);
+}
+
